9d436cd388dbdf5320141fce7e6c167f00765ab7
Unknownaod <unknown@dona.pw>
2026-09-16 22:43:20 -0400

    Change Logo size from 32 to 25

--- app/page.js
+++ app/page.js
@@ -2080,7 +2080,7 @@ export default function Home() {
           <div className="auth-card">
             <div className="auth-card-top">
               <div className="auth-pill">
-                <Logo size={32} />
+                <Logo size={25} />
                 <span>
                   Fades Mail
                 </span>
