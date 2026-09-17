ab20e482cb3a0bd64ab503dd5df16078124144d3
Unknownaod <unknown@dona.pw>
2026-09-16 21:22:22 -0400

    Fix formatting issues in recipient composer div

--- app/page.js
+++ app/page.js
@@ -3832,7 +3832,6 @@ export default function Home() {
     </button>
   )}
 </div>
-
                 <div
                   className={`recipient-composer ${
                     recipientSuggestionsOpen
